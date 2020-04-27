6a855dc2b4fe71b2deffc123a5528ffdf61b671a
AswathiMohan94 <aswathimohantly@gmail.com>
2020-04-27 07:17:53 +0530

    [Aswathi] ADDED : code and test cases to check equaltity of two feetVakues

---
+++ src/test/java/com/bridgelabz/QuantityMeasurementTest/QuantityTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.QuantityMeasurementTest;
+
+import com.bridgelabz.QuantityMeasurements.Feet;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuantityTest {
+    @Test
+    public void given0FeetAnd0FeetShouldReturnEqual(){
+        Feet feet1=new Feet(0.0);
+        Feet feet2=new Feet(0.0);
+        Assert.assertEquals(feet1,feet2);
+    }
+}
